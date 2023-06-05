c8fc98b29d4c9cf23051e3f5ee03f1fdb6f9dc73
DgekoTT <106529200+DgekoTT@users.noreply.github.com>
2023-06-05 12:57:23 +0300

    Update comments.controller.spec.ts

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
+import { CommentsDto } from './dto/comments.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Comments } from './comments.model';
+import { getModelToken } from '@nestjs/sequelize';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
-describe('CommentsController', () => {
-  let controller: CommentsController;
+describe('CommentsService', () => {
+    let commentsService: CommentsService;
+    let jwtService: JwtService;
+    let commentsModel: typeof Comments;
+    let comment = {
+        comment: 'здесь текст',
+        displayName: 'Peter',
+        filmId: 1,
+        parentCommentId: 2,
+    };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CommentsController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CommentsService,
+                JwtService,
+                {
+                    provide: getModelToken(Comments),
+                    useValue: {
+                        findAll: jest.fn(),
+                        create: jest.fn(),
+                        update: jest.fn(),
+                        destroy: jest.fn(),
+                        findAndCountAll: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-    controller = module.get<CommentsController>(CommentsController);
-  });
+        commentsService = module.get<CommentsService>(CommentsService);
+        jwtService = module.get<JwtService>(JwtService);
+        commentsModel = module.get<typeof Comments>(getModelToken(Comments));
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    describe('createComment', () => {
+        it('Должен создать новый комментарий', async () => {
+            const dto: CommentsDto =comment;
+            const refreshToken = 'token';
+            const user = { displayName: 'Peter' };
+
+            jest.spyOn(jwtService, 'verify').mockReturnValue(Promise.resolve(user));
+            jest.spyOn(commentsModel, 'create').mockReturnValue(Promise.resolve());
+
+            await expect(commentsService.createComment(dto, refreshToken)).resolves.toBeUndefined();
+
+            expect(jwtService.verify).toHaveBeenCalledWith(refreshToken, { secret: 'FFFGKJKFWMV' });
+            expect(commentsModel.create).toHaveBeenCalledWith(dto);
+        });
+
+        it('should throw an error if the comment creation fails', async () => {
+            const dto: CommentsDto = comment;
+            const refreshToken = 'token';
+            const user = { displayName: 'Peter' };
+
+            jest.spyOn(jwtService, 'verify').mockReturnValue(Promise.resolve(user));
+            jest.spyOn(commentsModel, 'create').mockRejectedValue(new HttpException('Comment creation failed', HttpStatus.INTERNAL_SERVER_ERROR));
+
+            await expect(() => commentsService.createComment(dto, refreshToken)).rejects.toThrow(HttpException);
+
+            expect(jwtService.verify).toHaveBeenCalledWith(refreshToken, { secret: 'FFFGKJKFWMV' });
+            expect(commentsModel.create).toHaveBeenCalledWith(dto);
+        });
+    });
+
 });
